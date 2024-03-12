45cd09f726383b8b026925a7599d232bb311f27a
rupszi <pal.megyes@hotmail.com>
2024-03-12 19:59:59 +0000

    fixed top menu line

--- 02-HTML-CSS-JS/singlepagewebsite/script.js
+++ 02-HTML-CSS-JS/singlepagewebsite/script.js
@@ -5,14 +5,14 @@ function addRecommendation() {
   if (recommendation.value != null && recommendation.value.trim() != "") {
     console.log("New recommendation added");
     //Call showPopup here
-
+    showPopup(true);
     // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
+    element.setAttribute("class", "recommendation");
     element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
     // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
+    document.getElementById("all_recommendations").appendChild(element);
+
     // Reset the value of the textarea
     recommendation.value = "";
   }
