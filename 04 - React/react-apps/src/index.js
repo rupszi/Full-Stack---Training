1b3e5d27033aadd58bcc83a67a0f9ef3078be92d
rupszi <pal.megyes@hotmail.com>
2024-03-16 19:31:26 +0000

    updated the font color and size in the index.js file

--- 04 - React/react-apps/src/index.js
+++ 04 - React/react-apps/src/index.js
@@ -5,4 +5,4 @@ import { createRoot } from 'react-dom/client';
 // This is how it it is done from React 18 onwards
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App color="green" size="20px" />);
+root.render(<App color="red" size="60px" />);
